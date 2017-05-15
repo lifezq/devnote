c1f6e70f0fc4ea2454b2117c4e1c2f9de2915dc1
Ryan <lifezqy@126.com>
2017-05-15 11:31:39 +0800

    atoi

--- cpp/strings.cpp
+++ cpp/strings.cpp
@@ -1,4 +1,6 @@
 #include<string.h>
+#include<limits.h>
+#include<stack>
 #include<iostream>
 using namespace std;
 
@@ -40,43 +42,62 @@ bool strBInA(const char* a, const char* b){
     return true;
 }
 
-char* reverseWord(char* s, char* delim){
-
-
-
-
-
-
+void reverseWord(char* s, char* delim){
 
     char str[strlen(s)];
 
     strcpy(str, s);
 
-    cout << "strlen:" << strlen(s) << " length:"   << endl;
 
     char *str02 = NULL;
     str02 = strtok(str, delim); 
+    
+    stack<char*> rst;
+    rst.push(str02);
 
-    cout << "str:" << str << endl;
-
-    char rst[10][];
-
-    int i=0;
     while(str02!=NULL){
-        cout << "str:" <<  " s:" << str02 << endl;
         str02 = strtok(NULL, delim);
 
-        rst[i] = str02;
-
-        i++;
-
-        cout << "rst:" << rst[i] << endl;
+        if(NULL != str02) rst.push(str02);
     }
 
+    int n=rst.size();
+    for(int i=0;i<n;i++){
+        cout << rst.top() << ", ";
+        rst.pop();
+    }
+    cout << endl;
+}
 
+int atoi_01(char* s){
 
-   //
-   return s;
+    if(NULL == s) return 0;
+    while(isspace(*s)) ++s;
+
+    int sign = 1;
+    if(s[0]=='+' || s[0] == '-'){
+        if(s[0] == '-') sign=-1;
+        ++s;
+    }
+
+    int n=0,c=0;
+    while(*s!=0){
+        if(!isdigit(*s)) return 0;
+        c=*s-'0';
+
+        if(sign==1 && (n>=INT_MAX/10 || (n==INT_MAX/10 && c>INT_MAX%10))){
+            n=INT_MAX;
+            break;
+        }else if(sign==-1 && (n>=INT_MAX/10 || (n==INT_MAX/10 && c-1>INT_MAX%10))){
+            n=INT_MIN;
+            break;
+        }
+
+       n=n*10+c;
+        ++s;
+    }
+
+    return sign*n;
 }
 
 int main(){
@@ -84,7 +105,9 @@ int main(){
     cout << "isPalindrome--------------------" << isPalindrome("madam", 5) << endl;
     cout << "strBInA-------------------------" << strBInA("abcdef","efa") << endl;
 
-    reverseWord("hello#you are best#now ok.", " ");
+    // reverseWord("hello world.", " ");
+    cout << "atoi_01--" << ((atoi_01("   +12345") == 12345) &&  (atoi_01("     -12345") == -12345)) << endl;
+    cout << "INT_MAX:" << atoi_01("  1233424235345344") << " INT_MIN:" << atoi_01("   -347289489398549");
     return 0;
 }
 
