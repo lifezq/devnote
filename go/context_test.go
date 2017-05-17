5d79a0ace4db0891d237a6deb3f1d184417f9131
Ryan <lifezqy@126.com>
2017-05-17 10:58:41 +0800

    context test..

---
+++ go/context_test.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestWithCancel(t *testing.T) {
+
+	// gen generates integers in a separate goroutine and
+	// sends them to the returned channel.
+	// The callers of gen need to cancel the context once
+	// they are done consuming generated integers not to leak
+	// the internal goroutine started by gen.
+	gen := func(ctx context.Context) <-chan int {
+		dst := make(chan int)
+		n := 1
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					return // returning not to leak the goroutine
+				case dst <- n:
+					n++
+
+				}
+
+			}
+
+		}()
+		return dst
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // cancel when we are finished consuming integers
+
+	for n := range gen(ctx) {
+		t.Log(n)
+		if n == 5 {
+			break
+
+		}
+
+	}
+}
+
+func TestDeadline(t *testing.T) {
+	d := time.Now().Add(50 * time.Millisecond)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+
+	// Even though ctx will be expired, it is good practice to call its
+	// cancelation function in any case. Failure to do so may keep the
+	// context and its parent alive longer than necessary.
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		t.Log("overslept")
+	case <-ctx.Done():
+		t.Error(ctx.Err())
+
+	}
+}
+
+func TestWithTimeout(t *testing.T) {
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		t.Log("overslept")
+	case <-ctx.Done():
+		t.Error(ctx.Err()) // prints "context deadline exceeded"
+
+	}
+
+}
+
+func TestWithValue(t *testing.T) {
+	type favContextKey string
+
+	f := func(ctx context.Context, k favContextKey) {
+		if v := ctx.Value(k); v != nil {
+			t.Log("found value:", v)
+			return
+
+		}
+		t.Log("key not found:", k)
+
+	}
+
+	k := favContextKey("language")
+	ctx := context.WithValue(context.Background(), k, "Go")
+
+	f(ctx, k)
+	f(ctx, favContextKey("color"))
+}
