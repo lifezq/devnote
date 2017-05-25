53a29c741c82b008f54201c1a8197e907cfa681a
Ryan <lifezqy@126.com>
2017-05-25 16:29:35 +0800

    socket max conn

--- cpp/socket_server.cpp
+++ cpp/socket_server.cpp
@@ -8,14 +8,23 @@
 #include <unistd.h>
 #include <time.h>
 
-#define SRV_PORT 8888
-#define SERVER_HELLO "Welcome to server! "
+#define SRV_PORT ((int)8888)
+#define SRV_HELLO "Welcome to server! "
+#define SRV_MAX_CONN ((int)2)
 
-int main(){
+int main(int argc, char **argv){
+
+    if(argc<=1){
+        printf("Usage:\n");
+        printf("Cmd [port(default:8888)] [max conn(default:2)]\n\n");
+    }
+
+    int port=argc>=2?atoi(argv[1]):SRV_PORT;
+    int maxConn=argc>=3?atoi(argv[2]):SRV_MAX_CONN;
 
     struct sockaddr_in addr;
-    int addr_len = sizeof(struct sockaddr_in);
-    char buffer[256];
+    socklen_t addr_len = sizeof(struct sockaddr_in);
+    char buffer[256], buf[30];
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd<0){
@@ -27,7 +36,7 @@ int main(){
 
     bzero(&addr, sizeof(addr));
     addr.sin_family=AF_INET;
-    addr.sin_port = htons(SRV_PORT);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr=htonl(INADDR_ANY);
 
     if(bind(sockfd, (const sockaddr*)&addr, sizeof(struct sockaddr_in))){
@@ -35,26 +44,26 @@ int main(){
         exit(1);
     }
 
-    if(listen(sockfd, 5)){
+    if(listen(sockfd, maxConn)){
         perror("listen");
         exit(1);
     }
 
-    printf("Listen on port:%d\n", SRV_PORT);
+    printf("Listen on port:%d\n", port);
 
     fd_set readfds;
-    int is_connected[10],  newsockfd;
-    for(int fd=0;fd<10;fd++) is_connected[fd]=0;
+    maxConn=maxConn+sockfd;
+    int is_connected[maxConn+1],  newsockfd;
+    for(int fd=sockfd;fd<=maxConn;fd++) is_connected[fd]=0;
 
     while(1){
 
         FD_ZERO(&readfds);
         FD_SET(sockfd, &readfds);
-        for(int fd=0;fd<10;fd++) if(is_connected[fd]) FD_SET(fd, &readfds);
-        if(!select (10, &readfds, NULL, NULL, NULL)) continue;
+        for(int fd=sockfd;fd<=maxConn;fd++) if(is_connected[fd]) FD_SET(fd, &readfds);
+        if(!select (maxConn+1, &readfds, NULL, NULL, NULL)) continue;
 
-
-        for(int fd=0;fd<10;fd++){
+        for(int fd=sockfd;fd<=maxConn;fd++){
 
             if(FD_ISSET(fd, &readfds)){
 
@@ -64,11 +73,19 @@ int main(){
                         continue;
                     }
 
+                    if(newsockfd > maxConn){
+                        bzero(buf, sizeof(buf));
+                        sprintf(buf, "Too many connections.\n", newsockfd);
+                        send(newsockfd, buf, sizeof(buf), 0 );
+                        shutdown(newsockfd, SHUT_RDWR);
+                        continue;
+                    }
+
                     time_t timep;
                     time(&timep);
                     char srv_hello[50];
                     bzero(srv_hello, sizeof(srv_hello));
-                    strcat(srv_hello, SERVER_HELLO);
+                    strcat(srv_hello, SRV_HELLO);
                     strcat(srv_hello,  asctime(gmtime(&timep)));
                     write(newsockfd, srv_hello, sizeof(srv_hello));
 
@@ -88,7 +105,6 @@ int main(){
 
                         if(strlen(buffer) && buffer[0]!='\n') printf("Recved Client[%d]: %s", fd, buffer);
 
-                        char buf[30];
                         bzero(buf, sizeof(buf));
                         if(strlen(buffer) && buffer[0]!='\n') sprintf(buf, "Client[%d] Message received.\n", fd);
 
