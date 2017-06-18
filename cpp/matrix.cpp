14dad93a1b4cbdadfb6cc94251c0536a1ed86597
Ryan <lifezqy@126.com>
2017-06-18 20:03:46 +0800

    Yang Matrix Search

--- cpp/matrix.cpp
+++ cpp/matrix.cpp
@@ -12,3 +12,35 @@ void mulMatrix(int **matrixA, int **matrixB, int **matrixC, int n){
     }
 }
 
+bool YangMatrixLocateSearch(int **matrix, int n, int value){
+    int i=0,j=n-1;
+    int val=matrix[i][j];
+    while(true){
+        if(val>value && j>0){
+           val=matrix[i][--j]; 
+        }else if(val<value && i<n-1){
+            val=matrix[++i][j];
+        }else if(val==value){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+bool YangMatrixBinarySearch(int **matrix, int n, int value){
+    int left=0,right=n-1;
+    while(left<=right){
+        int mid=left+((right-left)>>1);
+        if(matrix[mid][mid]>value){
+            right=mid-1;
+        }else if(matrix[mid][mid]<value){
+            left=mid+1;
+        }else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
