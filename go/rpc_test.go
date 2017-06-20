f02f8670a2054514c1364878f431137ad6a8da75
Ryan <lifezqy@126.com>
2017-06-20 16:33:31 +0800

    rpc test

---
+++ go/rpc_test.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"net"
+	"net/http"
+	"net/rpc"
+	"testing"
+)
+
+type Args struct {
+	A, B int
+}
+
+type Quotient struct {
+	Quo, Rem int
+}
+
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
+
+}
+
+func (t *Arith) Divide(args *Args, quo *Quotient) error {
+	if args.B == 0 {
+		return errors.New("divide by zero")
+
+	}
+	quo.Quo = args.A / args.B
+	quo.Rem = args.A % args.B
+	return nil
+}
+
+func TestRpc(t *testing.T) {
+
+	//server
+	arith := new(Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		t.Fatalf("listen error:", e)
+	}
+	go http.Serve(l, nil)
+
+	client, err := rpc.DialHTTP("tcp", ":1234")
+	if err != nil {
+		t.Fatalf("dialing:", err)
+	}
+
+	// Synchronous call
+	args := &Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		t.Fatalf("arith error:", err)
+	}
+
+	t.Logf("Arith: %d*%d=%d\n", args.A, args.B, reply)
+
+	// Asynchronous call
+	quotient := new(Quotient)
+	divCall := client.Go("Arith.Divide", args, quotient, nil)
+	<-divCall.Done // will be equal to divCall
+	// check errors, print, etc.
+
+	t.Logf("Quotient: %d/%d=%d %d%%%d=%d\n", args.A, args.B, quotient.Quo, args.A, args.B, quotient.Rem)
+}
